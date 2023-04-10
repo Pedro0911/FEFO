63cbb787859b20d7176f336d12caffb7af89dd79
João Pedro <pedrodossantos091121@gmail.com>
2023-04-10 11:18:30 -0300

    adicionado a função de criar a tabela
    foi adicionado a funcionalidade (incompleta) de criação de tabela. Ainda falata redirecionar esses dados para a página em que temos as tabela completa para donwload.
    
    -preciso tambem colocar um loop dentro da minha função, de modo em que o usuário possa adicionar quantas linhas quiser
    
    -preciso formatar o estilo da tabela
    -

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,37 @@
-let rua = document.getElementById('rua');
+var rua = document.getElementById('Rua');
 let codigo = document.getElementById('Cod');
-let quantidade = document.getElementById('qtd')
-let validade = document.getElementById('val')
+let quantidade = document.getElementById('qtd');
+let validade = document.getElementById('val');
 
-////abaixo terei as seções da lógica fuuncional da aolicação
+//codigo.value, rua.value,quantidade.value,validade.value
+//alert(codigo.value)
+
+//abaixo terei as seções da lógica fuuncional da aplicação
+
+////1- criar a função que captura os dados dos campos e adiciona a tabela/// 
+function add_produto(){ 
+    //criação da tabela antes de estar em algum loop
+    const tabela = document.createElement('table');
+    const tblCorpo = document.createElement('tbody');
+    const linha = document.createElement('tr');
+    const celula = document.createElement('rd');
+    
+    const celulaText = document.createTextNode(rua.value);
+    celula.appendChild(celulaText)
+
+    linha.appendChild(celula)
+    tblCorpo.appendChild(linha)
+
+    tabela.appendChild(tblCorpo)
+    document.body.appendChild(tabela);
+
+    tabela.setAttribute("border", "2");
+
+}
+
+
+
+
+/// 2- adicionar o loop para criar as linhas para cada confirmação de produto///
+
+/// função para fazerm donwload da tabela (pesquisa deve ser feita no stackoverflow)
