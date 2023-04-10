57a31667ad40bbc15549d004c403383408364d8b
João Pedro <pedrodossantos091121@gmail.com>
2023-04-10 09:26:52 -0300

    Adicionado o arquivo JS
    aqui comecei a criar a lógica funcional da aplicação. a parte de estilização será feita após o arquivo base já estiver concluído.

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+let rua = document.getElementById('rua');
+let codigo = document.getElementById('Cod');
+let quantidade = document.getElementById('qtd')
+let validade = document.getElementById('val')
+
+////abaixo terei as seções da lógica fuuncional da aolicação
