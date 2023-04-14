fc8a543c08e9722646cb5058572aa9214a3b61ae
João Pedro <pedrodossantos091121@gmail.com>
2023-04-14 11:45:19 -0300

    adicionando várias funcionalidade
    nesta atualização, foram adicionadas as funcionalidades de baixar a planilha. Também foram incluídos algumas formas de alerta de erro.
    
    no entanto ainda tem um pequeno bug que precisa ser resolvido.
    
    mas a aplicação em si já está 70% concluído

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,52 +3,77 @@ let codigo = document.getElementById('Cod');
 let quantidade = document.getElementById('qtd');
 let validade = document.getElementById('val');
 var alerta = document.getElementById('alerta');
-let new_produto
+let novo_produto = document.getElementById('new_produto')
 
 let botão = document.getElementById('new_produto')
+let baixar = document.getElementById('baixar_planilha') //botão de baixar a planilha
 
 var tabela = document.getElementsByClassName('my_tabela')[0]
 //codigo.value, rua.value,quantidade.value,validade.value, alerta.value
 //alert(codigo.value)
 
-//abaixo terei as seções da lógica fuuncional da aplicação
-
-////1- criar a função que captura os dados dos campos e adiciona a tabela/// 
-function add_produto(){ 
+////1- criar a função que captura os dados dos campos e adiciona a tabela 
+function add_produto(){
     var tabela = document.getElementById('tabela-main'); // conexão com a pagina que contem a tabela
     var linha = tabela.insertRow();
 
-    var celula1 = linha.insertCell(); //rua
-    var celula2 = linha.insertCell();// codigo
-    var celula3 = linha.insertCell();//quantidade
-    var celula4 = linha.insertCell();//validade
 
-    celula1.innerHTML = rua.value
-    celula2.innerHTML = codigo.value
-    celula3.innerHTML = quantidade.value
-    celula4.innerHTML = validade.value
+    if(
+    rua.value== ''||
+    codigo.value== ''||
+    quantidade.value== ''||
+    validade.value== ''
+    ){
+        alerta.innerText = 'Insira todos os campos' //alerta de confirmação da ação
+        alerta.style.color='#FF0000'
 
-    alerta.innerText = 'produto adicionado com sucesso !' //alerta de confirmação da ação
+        rua.style.borderBlockColor='#FF0000'
+        codigo.style.borderBlockColor='#FF0000'
+        quantidade.style.borderBlockColor='#FF0000'
+        validade.style.borderBlockColor='#FF0000'
+        
+
+    }else{
+        var celula1 = linha.insertCell(); //rua
+        var celula2 = linha.insertCell(); // codigo
+        var celula3 = linha.insertCell(); //quantidade
+        var celula4 = linha.insertCell(); //validade
+
+        celula1.innerHTML = rua.value
+        celula2.innerHTML = codigo.value
+        celula3.innerHTML = quantidade.value
+        celula4.innerHTML = validade.value
+        
+        alerta.innerText = 'produto adicionado com sucesso !' //alerta de confirmação da ação
+        alerta.style.color='4ee44e'
+
+        rua.style.borderBlockColor='#dee2e6'
+        codigo.style.borderBlockColor='#dee2e6'
+        quantidade.style.borderBlockColor='#dee2e6'
+        validade.style.borderBlockColor='#dee2e6'
+
+    }
+}
 
 
-    
-    //document.getElementsByClassName('my_tabela')[0].reset();
+function reset_form(){
 
-    //celula1.innerHTML = rua.value = " "
-    //celula2.innerHTML = codigo.value = " "
-    //celula3.innerHTML = quantidade.value = " "
-    //celula4.innerHTML =  validade.value = " "
+    document.getElementById('Rua').value= " "
+    document.getElementById('Cod').value= " "
+    document.getElementById('qtd').value= " "
+    document.getElementById('val').value= " "
+
+    alerta.innerText = ' ' 
 
 }
 
-function new_produto(){
-    //funcionalidades do botão amarelo
-    //botão esse que dixa os valores do formulário vazio
+
+
+
+function donwload(){
+    TableExport(document.getElementsByTagName("table")), {
+        filename: 'excelfile',            
+        sheetname: "sheet1"  
+    }
+
 }
-
-
-
-
-/// 2- adicionar o loop para criar as linhas para cada confirmação de produto///
-
-/// função para fazerm donwload da tabela (pesquisa deve ser feita no stackoverflow)
